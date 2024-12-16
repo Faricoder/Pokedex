d1d106842f8910468359d2fd0ddc2e148198ca24
erwan lorand <erwan.lorand@protonmail.com>
2024-12-16 16:11:05 +0100

    Challenge Q.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import "./App.css";
-
 import PokemonCard from "./components/PokemonCard";
 
+import "./App.css";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
-	return (
-		<div>
-			<PokemonCard />
-		</div>
-	);
+  return (
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
+    </div>
+  );
 }
 
 export default App;
