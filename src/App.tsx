0ead4f8145640e537a7b2298c5216090dd20975a
erwan lorand <erwan.lorand@protonmail.com>
2024-12-18 15:51:04 +0100

    Q7 Click button

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,48 @@ import PokemonCard from "./components/PokemonCard";
 import { useState } from "react";
 import "./App.css";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "charmander",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  },
-  {
-    name: "squirtle",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  },
-  {
-    name: "pikachu",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
-
-  const handleClick = () => {
-    setPokemonIndex(pokemonIndex + 1);
-    if (pokemonIndex < pokemonList.length - 1) return pokemonIndex;
-  };
-
-  const handlePrevious = () => {
-    setPokemonIndex(pokemonIndex - 1);
-    if (pokemonIndex > 0) return pokemonIndex;
-  };
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (
     <div>
+      <nav>
+        {pokemonList.map((pokemon, index) => (
+          <button
+            type="button"
+            key={pokemon.name}
+            onClick={() => setPokemonIndex(index)}
+          >
+            {pokemon.name}
+          </button>
+        ))}
+      </nav>
       <h1>Pokemon Index</h1>
-      <button type="button" onClick={handleClick}>
-        Suivant
-      </button>
-      <button type="button" onClick={handlePrevious}>
-        Précédent
-      </button>
 
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
